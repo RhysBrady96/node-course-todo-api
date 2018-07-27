1be06af9576b1b585d9557a105fb0c54134e3956
rhys brady <rhysbrady96@live.co.uk>
2018-07-27 15:50:19 +0100

    Added Tests for GET /todos Route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,14 +5,24 @@ const {app} = require("./../server");
 const {Todo} = require("./../models/Todo");
 
 
+// Dummy todos, act as seed data so we can test things like read, delete, and modify
+const todos = [
+    {text : "First test todo"},
+    {text : "Second test todo"},
+    {text : "Third test todo"}
+]
+
+
+
 // BeforeEach runs before EVERY TEST-CASE, only moves along once we call "done"
 beforeEach((done) => {
     // Removes all documents from the Todo collection
     Todo.remove({}).then(
         () => {
-            done()
+            // adds the dummy documents into the Collection
+            return Todo.insertMany(todos);
         }
-    )
+    ).then(() => done())
 });
 
 describe("POST /todos", () => {
@@ -34,7 +44,7 @@ describe("POST /todos", () => {
                     return done(err);
                 }
                 // Todo.find() <--- returns EVERY TODO in the collection
-                Todo.find().then( (todos) => {
+                Todo.find({text}).then( (todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -53,7 +63,7 @@ describe("POST /todos", () => {
                     return done(err);
                 }
                 Todo.find().then( (todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(3);
                     done();
                 }).catch((e) => done(e));
 
@@ -61,3 +71,15 @@ describe("POST /todos", () => {
             })
     })
 })
+
+describe("GET /todos", () => {
+    it("Should get ALL todos", (done) => {
+        request(app)
+            .get("/todos")
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(3);
+            })
+            .end(done);
+    })
+});
